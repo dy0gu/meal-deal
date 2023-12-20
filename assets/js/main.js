d2e9a369916bec0a5681281ef4a98958788593f5
Dy0gu <dbsimoes20@gmail.com>
2023-12-20 02:21:41 +0000

    Fix js formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,61 @@
 /*=============== GET COPYRIGHT YEAR ===============*/
 var currentYear = new Date().getFullYear();
-document.getElementById('copyright').textContent = '© ' + currentYear + ' MealDeal. All rights reserved.';
+document.getElementById("copyright").textContent = "© " + currentYear + " MealDeal. All rights reserved.";
 
 /*=============== FORM SUBMISSION ===============*/
 function submitForm(event) {
     event.preventDefault();
 
     // Clear input fields and do nothing
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
+    document.getElementById("form-email").value = "";
+    document.getElementById("form-subject").value = "";
+    document.getElementById("form-message").value = "";
 }
 
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close")
 
 /*===== MENU SHOW =====*/
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
     })
 }
 
 /*===== MENU HIDDEN =====*/
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
     })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link")
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+    const navMenu = document.getElementById("nav-menu")
+    navMenu.classList.remove("show-menu")
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener("click", linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
-    const header = document.getElementById('header')
+    const header = document.getElementById("header")
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 80) header.classList.add("scroll-header"); else header.classList.remove("scroll-header")
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader)
 
 /*=============== QUESTIONS ACCORDION ===============*/
-const accordionItems = document.querySelectorAll('.questions__item')
+const accordionItems = document.querySelectorAll(".questions__item")
 
 accordionItems.forEach((item) => {
-    const accordionHeader = item.querySelector('.questions__header')
+    const accordionHeader = item.querySelector(".questions__header")
 
-    accordionHeader.addEventListener('click', () => {
-        const openItem = document.querySelector('.accordion-open')
+    accordionHeader.addEventListener("click", () => {
+        const openItem = document.querySelector(".accordion-open")
 
         toggleItem(item)
 
@@ -66,21 +66,21 @@ accordionItems.forEach((item) => {
 })
 
 const toggleItem = (item) => {
-    const accordionContent = item.querySelector('.questions__content')
+    const accordionContent = item.querySelector(".questions__content")
 
-    if (item.classList.contains('accordion-open')) {
-        accordionContent.removeAttribute('style')
-        item.classList.remove('accordion-open')
+    if (item.classList.contains("accordion-open")) {
+        accordionContent.removeAttribute("style")
+        item.classList.remove("accordion-open")
     } else {
-        accordionContent.style.height = accordionContent.scrollHeight + 'px'
-        item.classList.add('accordion-open')
+        accordionContent.style.height = accordionContent.scrollHeight + "px"
+        item.classList.add("accordion-open")
     }
 
 }
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section[id]')
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section[id]")
 
     function scrollActive() {
         const scrollY = window.scrollY
@@ -88,60 +88,60 @@ document.addEventListener('DOMContentLoaded', function () {
         sections.forEach(current => {
             const sectionHeight = current.offsetHeight,
                 sectionTop = current.offsetTop - 58,
-                sectionId = current.getAttribute('id'),
-                link = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+                sectionId = current.getAttribute("id"),
+                link = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
 
             if (link) {
                 if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    link.classList.add('active-link');
+                    link.classList.add("active-link");
                 } else {
-                    link.classList.remove('active-link');
+                    link.classList.remove("active-link");
                 }
             }
         })
     }
 
-    window.addEventListener('scroll', scrollActive);
+    window.addEventListener("scroll", scrollActive);
 });
 
 
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
-    const scrollUp = document.getElementById('scroll-up');
+    const scrollUp = document.getElementById("scroll-up");
     // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 400) scrollUp.classList.add("show-scroll"); else scrollUp.classList.remove("show-scroll")
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+const themeButton = document.getElementById("theme-button")
+const darkTheme = "dark-theme"
+const iconTheme = "ri-sun-line"
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme")
+const selectedIcon = localStorage.getItem("selected-icon")
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line"
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
+    themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+    localStorage.setItem("selected-theme", getCurrentTheme())
+    localStorage.setItem("selected-icon", getCurrentIcon())
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
+    origin: "top",
+    distance: "60px",
     duration: 2500,
     delay: 400,
 })
@@ -149,6 +149,6 @@ const sr = ScrollReveal({
 sr.reveal(`.home__data`)
 sr.reveal(`.home__img`, { delay: 500 })
 sr.reveal(`.home__social`, { delay: 600 })
-sr.reveal(`.about__img, .contact__box`, { origin: 'left' })
-sr.reveal(`.about__data, .contact__form`, { origin: 'right' })
+sr.reveal(`.about__img, .contact__box`, { origin: "left" })
+sr.reveal(`.about__data, .contact__form`, { origin: "right" })
 sr.reveal(`.steps__card, .members__card, .questions__group, .footer`, { interval: 100 })
